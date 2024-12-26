c048684304be788f9f9a9469966b8245db9eb785
abdulrahemfaqih <faqih3935@gmail.com>
2024-12-26 19:20:52 +0700

    initial init

---
+++ app/Database/Migrations/2024-12-26-103244_CreateUsersTable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+use function PHPSTORM_META\type;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            "id" => [
+                "type" => "INT",
+                "constraint" => 11,
+                "unsigned" => true,
+                "auto_increment" => true,
+            ],
+            "name" => [
+                "type" => "VARCHAR",
+                "constraint" => 100,
+            ],
+            "username" => [
+                "type" => "VARCHAR",
+                "constraint" => 100,
+            ],
+            "email" => [
+                "type" => "VARCHAR",
+                "constraint" => 100,
+            ],
+            "password" => [
+                "type" => "VARCHAR",
+                "constraint" => 255,
+            ],
+            "picture" => [
+                "type" => "VARCHAR",
+                "constraint" => 255,
+                "null" => true
+            ],
+            "bio" => [
+                "type" => "TEXT",
+                "null" => true
+            ],
+            "created_at timestamp default current_timestamp",
+            "updated_at timestamp default current_timestamp on update current_timestamp"
+        ]);
+    }
+
+    public function down()
+    {
+        //
+    }
+}
